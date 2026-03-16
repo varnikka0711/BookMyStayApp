f5a72440322de301a60856d85b358f5ac08c3e77
varnikka0711 <varnikka0711@gmail.com>
2026-03-16 14:40:37 +0530

    add: UC3

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,41 +1,70 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hotel Room Initialization\n");
+        System.out.println("Hotel Room Inventory Status\n");
 
         // Create room objects
         Room singleRoom = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
         Room suiteRoom = new SuiteRoom();
 
-        // Static availability variables
-        int singleRoomAvailable = 5;
-        int doubleRoomAvailable = 3;
-        int suiteRoomAvailable = 2;
+        // Initialize centralized inventory
+        RoomInventory inventory = new RoomInventory();
+
+        // Get availability map
+        Map<String, Integer> availability = inventory.getRoomAvailability();
 
         // Display Single Room
         System.out.println("Single Room:");
         singleRoom.displayRoomDetails();
-        System.out.println("Available: " + singleRoomAvailable + "\n");
+        System.out.println("Available Rooms: " + availability.get("SingleRoom") + "\n");
 
         // Display Double Room
         System.out.println("Double Room:");
         doubleRoom.displayRoomDetails();
-        System.out.println("Available: " + doubleRoomAvailable + "\n");
+        System.out.println("Available Rooms: " + availability.get("DoubleRoom") + "\n");
 
         // Display Suite Room
         System.out.println("Suite Room:");
         suiteRoom.displayRoomDetails();
-        System.out.println("Available: " + suiteRoomAvailable);
+        System.out.println("Available Rooms: " + availability.get("SuiteRoom"));
+    }
+}
+
+class RoomInventory {
+
+    private Map<String, Integer> roomAvailability;
+
+    public RoomInventory() {
+        roomAvailability = new HashMap<>();
+        initializeInventory();
+    }
+
+    private void initializeInventory() {
+        roomAvailability.put("SingleRoom", 5);
+        roomAvailability.put("DoubleRoom", 3);
+        roomAvailability.put("SuiteRoom", 2);
+    }
+
+    public Map<String, Integer> getRoomAvailability() {
+        return roomAvailability;
+    }
+
+    public void updateAvailability(String roomType, int count) {
+        roomAvailability.put(roomType, count);
     }
 }
 
 abstract class Room {
+
     protected int numberOfBeds;
     protected int squareFeet;
     protected double pricePerNight;
+
     public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
         this.numberOfBeds = numberOfBeds;
         this.squareFeet = squareFeet;
@@ -50,18 +79,34 @@ abstract class Room {
     }
 }
 
+
+/**
+ * CLASS - SingleRoom
+ */
 class SingleRoom extends Room {
+
     public SingleRoom() {
         super(1, 250, 1500.0);
     }
 }
 
+
+/**
+ * CLASS - DoubleRoom
+ */
 class DoubleRoom extends Room {
+
     public DoubleRoom() {
         super(2, 400, 2500.0);
     }
 }
+
+
+/**
+ * CLASS - SuiteRoom
+ */
 class SuiteRoom extends Room {
+
     public SuiteRoom() {
         super(3, 750, 5000.0);
     }
